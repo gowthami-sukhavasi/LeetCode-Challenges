0e297117e5a3b6995412ac724b1e8c08ec1e3c58
Gowthami Sukhavasi <gowthamisukhavasi@yahoo.in>
2020-06-03 15:47:41 +0530

    Update Single Number.cpp

--- 30-Day LeetCoding Challenge/Week 1: April 1st–April 7th/Day 1/Single Number.cpp
+++ 30-Day LeetCoding Challenge/Week 1: April 1st–April 7th/Day 1/Single Number.cpp
@@ -1,1 +1,28 @@
+/*
+Approach:
+1. XOR each element in the array and save in a variable
+2. This variable will contain the only non repeating number.
+3. return this variable. 
+*/
 
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sum=0;
+        for(int i = 0;i<nums.size();i++)
+            sum^=nums[i];
+        return sum;
+    }
+};
+ 
+int main()
+{
+    Solution obj;
+    vector<int> vec = {4,1,2,1,2};
+    cout<<obj.singleNumber(vec);
+    return 0;
+}
